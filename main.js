ecdbc1780edbdd0b816702592ad3d092a6e18158
Satit Rianpit <rianpit@gmail.com>
2016-10-17 23:42:51 +0700

    change entry point

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/login.html`)
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
--- src/js/configure.js
+++ src/js/configure.js
@@ -8,10 +8,10 @@ module.exports = {
   },
 
   getHOSxPConnectionPool() {
-    let HOS_HOST = config.get('HOS_HOST') || '172.16.0.3';
+    let HOS_HOST = config.get('HOS_HOST') || 'localhost';
     let HOS_DBNAME = config.get('HOS_DBNAME') || 'hos';
     let HOS_USER = config.get('HOS_USER') || 'sa';
-    let HOS_PASSWORD = config.get('HOS_PASSWORD') || '0437891241';
+    let HOS_PASSWORD = config.get('HOS_PASSWORD') || 'sa';
     let HOS_PORT = config.get('HOS_PORT') || '3306';
  
     let pool = mysql.createPool({
@@ -30,10 +30,10 @@ module.exports = {
   },
 
   getHDCConnectionPool() {
-    let HDC_HOST = config.get('HDC_HOST') || '172.16.0.6';
+    let HDC_HOST = config.get('HDC_HOST') || 'localhost';
     let HDC_DBNAME = config.get('HDC_DBNAME') || 'hdc';
     let HDC_USER = config.get('HDC_USER') || 'hdc';
-    let HDC_PASSWORD = config.get('HDC_PASSWORD') || '043789124';
+    let HDC_PASSWORD = config.get('HDC_PASSWORD') || 'hdc';
     let HDC_PORT = config.get('HDC_PORT') || '3306';
  
     let pool = mysql.createPool({
